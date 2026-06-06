339038cb0e11cc714c8d2a0b68a156f8f823ef99
Wim Goossens <291154462+wimgoossens-dev@users.noreply.github.com>
2026-06-06 02:21:04 +0200

    Storing calculations in variables.

---
+++ storing_in_variables.py
@@ -0,0 +1,13 @@
+word = "Hello"
+name = "World"
+sentence = word + " " + name
+print(sentence)
+
+word = "Goodbye"
+
+'''
+Unlike a spreadsheet where formulas update automatically,
+a variable like sentence doesn't remember how it was calculated
+and won't change if the underlying values word or name are changed.
+'''
+print(sentence)
