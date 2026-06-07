6e566cc08471b840d2ca668f535ba85296a7234d
Wim Goossens <291154462+wimgoossens-dev@users.noreply.github.com>
2026-06-07 22:21:01 +0200

    Decorate users name with a box around it.

--- building_up_strings.py
+++ building_up_strings.py
@@ -4,3 +4,20 @@ line = ""
 for char in name:
     line = line + char
     print(line)
+
+name = "World"
+line = ""
+
+for char in name:
+    line = char + line
+    print(line)
+
+name = input('Please enter your name so I can decorate it properly: ')
+line = ''
+
+for char in name:
+    line += '-'
+        
+print('+' + line + '+')
+print('|' + name + '|')    
+print('+' + line + '+')
