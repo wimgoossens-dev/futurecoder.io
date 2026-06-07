2cc71d45000cbb66bbe99e4ac638ff5aa6ae4cfc
Wim Goossens <291154462+wimgoossens-dev@users.noreply.github.com>
2026-06-07 22:46:24 +0200

    Exercises on building up strings.

--- building_up_strings.py
+++ building_up_strings.py
@@ -1,3 +1,4 @@
+# Modify this program
 name = "World"
 line = ""
 
@@ -5,6 +6,7 @@ for char in name:
     line = line + char
     print(line)
 
+# Program it backwards
 name = "World"
 line = ""
 
@@ -12,12 +14,33 @@ for char in name:
     line = char + line
     print(line)
 
-name = input('Please enter your name so I can decorate it properly: ')
-line = ''
+# Extend your program to draw a box around the name
+name = input("Please enter your name so I can decorate it properly: ")
+line = ""
 
 for char in name:
-    line += '-'
+    line += "-"
         
-print('+' + line + '+')
-print('|' + name + '|')    
-print('+' + line + '+')
+print("+" + line + "+")
+print("|" + name + "|")    
+print("+" + line + "+")
+
+# Now the name should be the box
+line = ""
+
+for char in name:
+    line += " "
+        
+print("+" + name + "+")
+
+for char in name:
+    print(char + line + char)
+    
+print("+" + name + "+")
+
+# Write the name diagonal
+line = ""
+
+for char in name:
+    print(line + char)
+    line += " "
