9802c3a5266d8b44f1fe521cf2b6b8e50cc7274b
Wim Goossens <291154462+wimgoossens-dev@users.noreply.github.com>
2026-06-08 23:29:49 +0200

    Understanding Programs With <<snoop>> is like working with a debugger. It provides a line-by-line execution log and variable tracking via decorators or context managers.

---
+++ Understanding_with_snoop.py
@@ -0,0 +1,23 @@
+# Snoop is the debugger inside futurecoder.io
+sentence = 'Hello World'
+
+include = False
+new_sentence = ''
+for char in sentence:
+    if include:
+        new_sentence += char
+    include = True
+
+print(new_sentence)
+
+# This will output the exact opposite
+sentence = 'Hello World'
+
+include = True
+new_sentence = ''
+for char in sentence:
+    if include:
+        new_sentence += char
+    include = False
+
+print(new_sentence)
