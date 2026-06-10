7aedf2e75ae2cf585cfd18f5e55bbff31fc5fc2f
Wim Goossens <291154462+wimgoossens-dev@users.noreply.github.com>
2026-06-10 23:58:16 +0200

    Building new lists.

---
+++ break_to_end_loop.py
@@ -0,0 +1,29 @@
+# Exercise
+things = ["This", "is", "a", "list"]
+thing_to_find = "is"
+
+found = False
+for thing in things:
+    if thing == thing_to_find:
+        found = True
+
+print(found)
+
+things = ["This", "is", "a", "list"]
+thing_to_find = "other"
+
+found = False
+for thing in things:
+    if thing == thing_to_find:
+        found = True
+
+print(found)
+
+# Optimized solution
+things = ["This", "is", "a", "list"]
+thing_to_find = "other"
+
+for thing in things:
+    if thing == thing_to_find:
+        found = True
+        break
