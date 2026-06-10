f22fe9a5fe276bdcacfbf242490ba3198b06f64a
Wim Goossens <291154462+wimgoossens-dev@users.noreply.github.com>
2026-06-10 23:20:40 +0200

    Building new lists.

--- building_new_lists.py
+++ building_new_lists.py
@@ -1,1 +1,16 @@
+# First exercise
+numbers = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1028]
+doubled = []
 
+for number in numbers:
+    doubled += [number + number]
+print(doubled)
+
+# My solution to the second exercise
+numbers = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1028]
+greaterThanFiveList = []
+for number in numbers:
+    if number > 5:
+        greaterThanFiveList.append(number)
+
+print(greaterThanFiveList)
