f793bbeabcbbd530fe482e55c2d82796f27ba170
wimgoossens-dev <wim.goossens@mailfence.com>
2026-08-03 23:51:36 +0200

    Combining and and or booleans

--- and_or_booleans.py
+++ and_or_booleans.py
@@ -3,4 +3,44 @@ def assert_equal(actual, expected):
     if actual == expected:
         print("OK")
     else:
-        print(f"Error! {repr(actual)} != {repr(expected)}")
+        print(f"Error! {actual!r} != {expected!r}")
+
+def diagonal_winner(board):
+    middle = board[1][1]
+    return (
+            (middle == board[0][0] and middle == board[2][2]) or
+            (middle == board[0][2] and middle == board[2][0])
+    )
+
+assert_equal(
+    diagonal_winner(
+        [
+            ['X', 'O', 'X'],
+            ['X', 'X', 'O'],
+            ['O', 'O', 'X']
+        ]
+    ),
+    True
+)
+
+assert_equal(
+    diagonal_winner(
+        [
+            ['X', 'X', 'O'],
+            ['X', 'O', 'O'],
+            ['O', 'X', 'X']
+        ]
+    ),
+    True
+)
+
+assert_equal(
+    diagonal_winner(
+        [
+            ['O', 'X', 'O'],
+            ['X', 'X', 'X'],
+            ['O', 'O', 'X']
+        ]
+    ),
+    False
+)
